79b652dd9bea61ca6f7cea40fedc801374e8f3ad
FabianBG <fbastidas@latamautos.com>
2019-07-18 17:15:17 -0500

    Simple layout to send images

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { View, Text, Image, Button } from 'react-native'
+import { View, Text, Image, Button, FlatList, StyleSheet } from 'react-native'
 import ImagePicker from 'react-native-image-picker'
 
 export default class App extends React.Component {
   state = {
     photo: null,
+    text: []
   }
 
   render() {
-    const { photo } = this.state
+    const { photo, text } = this.state
+    const styles = StyleSheet.create({
+      container: {
+       flex: 1,
+       paddingTop: 22
+      },
+    })
+    
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         {photo && (
@@ -17,10 +25,15 @@ export default class App extends React.Component {
               source={{ uri: photo.uri }}
               style={{ width: 300, height: 300 }}
             />
-            <Button title="Upload" onPress={this.handleUpload} />
+            
+            <Button title="ENVIAR" onPress={this.handleUploadPhoto} />
           </React.Fragment>
         )}
-        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+        <Button title="ESCOJE UNA IMAGEN" onPress={this.handleChoosePhoto} style={styles.container}/>
+        <FlatList
+          data={text}
+          renderItem={({item}) => <Text>{item[0]} - {item[1]}</Text>}
+        />
       </View>
     )
   }
@@ -46,23 +59,27 @@ export default class App extends React.Component {
     const options = {
       noData: true,
     }
+    console.info("Buscando data....");
+
     ImagePicker.launchImageLibrary(options, response => {
       if (response.uri) {
-        this.setState({ photo: response })
+        this.setState({ photo: response, text: [] })
       }
     })
   }
 
   handleUploadPhoto = () => {
-    fetch("http://localhost:3000/api/upload", {
+    console.info("Enviadno data....");
+    this.setState({ ...this.state, text: [["1", "Analisando data"]]});
+    fetch("https://7cb41cbf.ngrok.io/api", {
       method: "POST",
-      body: createFormData(this.state.photo, { model: "modelname" })
+      body: this.createFormData(this.state.photo, { model: "fashion-color.pb:predict" })
     })
       .then(response => response.json())
       .then(response => {
         console.log("upload succes", response);
-        alert("Upload success!");
-        this.setState({ photo: null });
+        alert("Analisis finalizado ...");
+        this.setState({ ...this.state, text: response.predictions});
       })
       .catch(error => {
         console.log("upload error", error);
