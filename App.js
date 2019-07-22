c362a67d2a464783db10db66f86eec83e791a5d3
FabianBG <fbastidas@latamautos.com>
2019-07-22 16:47:55 -0500

    Add google url

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { View, Text, Image, Button, FlatList, StyleSheet } from 'react-native'
+import { View, Text, Image, Button, FlatList, StyleSheet, Dimensions } from 'react-native'
+import { WebView } from 'react-native-webview';
 import ImagePicker from 'react-native-image-picker'
 
 export default class App extends React.Component {
   state = {
     photo: null,
-    text: []
+    text: [],
+    mostrar: 'res'
   }
 
   render() {
-    const { photo, text } = this.state
+    const { photo, text, mostrar } = this.state
     const styles = StyleSheet.create({
       container: {
        flex: 1,
@@ -19,23 +21,52 @@ export default class App extends React.Component {
     
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
         {photo && (
           <React.Fragment>
             <Image
               source={{ uri: photo.uri }}
-              style={{ width: 300, height: 300 }}
+              style={{ width: 200, height: 200 }}
             />
             
             <Button title="ENVIAR" onPress={this.handleUploadPhoto} />
           </React.Fragment>
         )}
         <Button title="ESCOJE UNA IMAGEN" onPress={this.handleChoosePhoto} style={styles.container}/>
-        <FlatList
+        <Button title={mostrar == 'web'? 'RESULTADOS': 'WEB'} onPress={this.handleChangeView} style={styles.container}/>
+        { mostrar == 'res' && (<FlatList
           data={text}
           renderItem={({item}) => <Text>{item[0]} - {item[1]}</Text>}
-        />
+        />)}
+        { mostrar == 'web' && this.renderWebView()}
+        
       </View>
     )
+  }
+
+  renderWebView() {
+    let jsCode = `window.scrollTo(10, 200)`;
+    var {height, width} = Dimensions.get('window');
+    const { text } = this.state
+    const label1 = text[0][1]
+    const label2 = text[1][1]
+    const uri = `https://www.google.com/search?source=lnms&tbm=isch&sa=X&ved=0ahUKEwjts_PtsMnjAhVSs1kKHe2SCXEQ_AUIESgB&biw=1402&bih=858&q=${label1}+${label2}`
+    console.info("VISIT", uri)
+    return <WebView
+    style={{
+      maxHeight: 400,
+      width: width,
+      flex: 1
+    }}
+    //Loading URL
+    source={{ uri }}
+    injectedJavaScript={jsCode}
+    //Enable Javascript support
+    javaScriptEnabled={true}
+    //For the Cache
+    domStorageEnabled={true}
+    onLoadStart={() => console.info("CARGANDO PAGINA")}
+    onLoad={() => console.info("CARGADA PAGINA")}/>
   }
 
   createFormData = (photo, body) => {
@@ -63,7 +94,7 @@ export default class App extends React.Component {
 
     ImagePicker.launchImageLibrary(options, response => {
       if (response.uri) {
-        this.setState({ photo: response, text: [] })
+        this.setState({ photo: response, text: [], mostar: "res" })
       }
     })
   }
@@ -71,19 +102,26 @@ export default class App extends React.Component {
   handleUploadPhoto = () => {
     console.info("Enviadno data....");
     this.setState({ ...this.state, text: [["1", "Analisando data"]]});
-    fetch("https://7cb41cbf.ngrok.io/api", {
+    fetch("https://up-company.dev/dev-parca/api", {
       method: "POST",
       body: this.createFormData(this.state.photo, { model: "fashion-color.pb:predict" })
     })
       .then(response => response.json())
       .then(response => {
-        console.log("upload succes", response);
+        console.log("upload success", response);
         alert("Analisis finalizado ...");
-        this.setState({ ...this.state, text: response.predictions});
+        this.setState({ ...this.state, text: response.predictions, mostar: "web"});
       })
       .catch(error => {
         console.log("upload error", error);
-        alert("Upload failed!");
+        alert("Upload failed!" + error);
       });
   };
+
+  handleChangeView = () => {
+    this.setState({
+      ...this.state,
+      mostrar: this.state.mostrar == "web" ? "res":"web"
+    })
+  }
 }
